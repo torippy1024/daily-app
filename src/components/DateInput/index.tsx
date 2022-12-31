af6f59366ff253f22d45373205d09a5e6ddbb348
h.toriyama <toriyamahiroto1128@gmail.com>
2023-01-01 00:20:22 +0900

    日付入力をコンポーネント化

---
+++ src/components/DateInput/index.tsx
@@ -0,0 +1,39 @@
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import {Dispatch, SetStateAction} from 'react';
+import {format} from 'date-fns';
+import {AiOutlineCalendar} from 'react-icons/ai';
+
+type DateInputType = {
+  date: Date;
+  setDate: Dispatch<SetStateAction<Date>>;
+};
+
+const DateInput = ({date, setDate}: DateInputType) => {
+  const handleChange = (date: Date) => {
+    setDate(date);
+  };
+
+  const formatDate = (date: Date) => {
+    return format(date, 'yyyy/MM/dd');
+  };
+
+  return (
+    <ReactDatePicker
+      selected={date}
+      onChange={handleChange}
+      customInput={
+        <div className='inline-flex items-stretch justify-start cursor-pointer'>
+          <div className='text-2xl border rounded-l-xl border-gray-400 bg-base-100 p-2'>
+            {formatDate(date)}
+          </div>
+          <div className='border rounded-r-xl border-primary flex items-center p-2 text-base-100 bg-primary'>
+            <AiOutlineCalendar size={24} />
+          </div>
+        </div>
+      }
+    />
+  );
+};
+
+export default DateInput;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,13 @@
 import type {NextPage} from 'next';
 import Layout from '../components/layouts/Layout';
-import ReactDatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import {useState} from 'react';
-import {format} from 'date-fns';
-import {AiOutlineCalendar} from 'react-icons/ai';
+import DateInput from '../components/DateInput';
 
 const Home: NextPage = () => {
   const [date, setDate] = useState(new Date());
-
-  const handleChange = (date: Date) => {
-    setDate(date);
-  };
-
-  const formatDate = (date: Date) => {
-    return format(date, 'yyyy/MM/dd');
-  };
-
   return (
     <Layout>
-      <ReactDatePicker
-        selected={date}
-        onChange={handleChange}
-        customInput={
-          <div className='inline-flex items-stretch justify-start cursor-pointer'>
-            <div className='text-2xl border rounded-l-xl border-gray-400 bg-base-100 p-2'>
-              {formatDate(date)}
-            </div>
-            <div className='border rounded-r-xl border-primary flex items-center p-2 text-base-100 bg-primary'>
-              <AiOutlineCalendar size={24} />
-            </div>
-          </div>
-        }
-      />
+      <DateInput date={date} setDate={setDate} />
     </Layout>
   );
 };
