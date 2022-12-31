9506c71d945da884215adead4d9ea8a59b7d1ee4
h.toriyama <toriyamahiroto1128@gmail.com>
2022-12-31 23:15:06 +0900

    react-datepickerを導入

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import type {NextPage} from 'next';
 import Link from 'next/link';
 import Layout from '../components/layouts/Layout';
+import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import {useState} from 'react';
 
 const Home: NextPage = () => {
+  const [date, setDate] = useState(new Date());
+  const handleChange = (date: Date) => {
+    setDate(date);
+  };
+
   return (
     <Layout>
-      <div>hogehoge</div>
-      <div className='btn btn-primary'>
-        <Link href='/page2'>link to page2</Link>
-      </div>
+      <ReactDatePicker
+        selected={date}
+        onChange={handleChange}
+        customInput={<div>{date.toISOString().split('T')[0]}</div>}
+      />
     </Layout>
   );
 };
