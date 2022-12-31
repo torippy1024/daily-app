1b1b4bdcdaa475910f935ab669d287598a08c691
h.toriyama <toriyamahiroto1128@gmail.com>
2023-01-01 00:07:10 +0900

    日付入力の見た目をきれいに

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import type {NextPage} from 'next';
-import Link from 'next/link';
 import Layout from '../components/layouts/Layout';
 import ReactDatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import {useState} from 'react';
+import {format} from 'date-fns';
+import {AiOutlineCalendar} from 'react-icons/ai';
 
 const Home: NextPage = () => {
   const [date, setDate] = useState(new Date());
+
   const handleChange = (date: Date) => {
     setDate(date);
+  };
+
+  const formatDate = (date: Date) => {
+    return format(date, 'yyyy/MM/dd');
   };
 
   return (
@@ -16,7 +22,16 @@ const Home: NextPage = () => {
       <ReactDatePicker
         selected={date}
         onChange={handleChange}
-        customInput={<div>{date.toISOString().split('T')[0]}</div>}
+        customInput={
+          <div className='inline-flex items-stretch justify-start cursor-pointer'>
+            <div className='text-2xl border rounded-l-xl border-gray-400 bg-base-100 p-2'>
+              {formatDate(date)}
+            </div>
+            <div className='border rounded-r-xl border-primary flex items-center p-2 text-base-100 bg-primary'>
+              <AiOutlineCalendar size={24} />
+            </div>
+          </div>
+        }
       />
     </Layout>
   );
