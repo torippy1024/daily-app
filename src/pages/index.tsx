94ba24895634de89b8e83d9e2bae20abadf99475
h.toriyama <toriyamahiroto1128@gmail.com>
2023-01-01 08:54:04 +0900

    複数リンクを開く

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,32 @@
 import type {NextPage} from 'next';
 import Layout from '../components/layouts/Layout';
-import {useState} from 'react';
+import {useState, MouseEventHandler} from 'react';
 import DateInput from '../components/DateInput';
 
 const Home: NextPage = () => {
   const [date, setDate] = useState(new Date());
+
+  const sites = [
+    {
+      name: 'photo',
+      url: 'https://photos.google.com/u/1/search/_d20221225_',
+    },
+    {
+      name: 'calendar',
+      url: 'https://calendar.google.com/calendar/u/0/r/day/2022/12/26',
+    },
+  ];
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+    sites.map((site) => {
+      window.open(site.url, '_blank');
+    });
+  };
+
   return (
     <Layout>
       <DateInput date={date} setDate={setDate} />
+      <button onClick={handleClick}>hoge</button>
     </Layout>
   );
 };
