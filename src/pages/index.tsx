6a8c3a99c20cf0d65fefddd098ea1d7ddbe4edfb
h.toriyama <toriyamahiroto1128@gmail.com>
2023-01-01 11:38:39 +0900

    SitesContainerを作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,11 @@
 import type {NextPage} from 'next';
 import Layout from '../components/layouts/Layout';
-import {useState, MouseEventHandler} from 'react';
-import DateInput from '../components/DateInput';
-import useSites from '../hooks/useSites';
+import SitesContainer from '../components/sites';
 
 const Home: NextPage = () => {
-  const [date, setDate] = useState(new Date());
-  const sites = useSites(date);
-
-  const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
-    sites.map((site) => {
-      window.open(site.url, '_blank');
-    });
-  };
-
   return (
     <Layout>
-      <DateInput date={date} setDate={setDate} />
-      <button className='btn my-2' onClick={handleClick}>
-        Open Links
-      </button>
+      <SitesContainer />
     </Layout>
   );
 };
